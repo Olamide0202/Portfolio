8ae6e3f7c51bdedf8282c12672d19eeede5ff1c8
unknown <Cornerstone>
2025-02-10 16:02:03 +0100

    changes to portfolio

---
+++ src/Details.js
@@ -0,0 +1,176 @@
+// Enter all your detials in this file
+// Logo images
+import logogradient from "./assets/logo.png";
+import logo from "./assets/logo2.png";
+// Profile Image
+import profile from "./assets/profile.jpg";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import sass from "./assets/techstack/sass.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import redux from "./assets/techstack/redux.png";
+import tailwind from "./assets/techstack/tailwind.png";
+import bootstrap from "./assets/techstack/bootstrap.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+import postman from "./assets/techstack/postman.png";
+import figma from "./assets/techstack/figma.png";
+// Porject Images
+import projectImage1 from "./assets/projects/project1.jpg";
+import projectImage2 from "./assets/projects/project2.jpg";
+import projectImage3 from "./assets/projects/project3.jpg";
+import projectImage4 from "./assets/projects/project4.jpg";
+import projectImage5 from "./assets/projects/project5.jpg";
+import projectImage6 from "./assets/projects/project6.jpg";
+
+// Logos
+export const logos = {
+  logogradient: logogradient,
+  logo: logo,
+};
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "Samuel Bello",
+  tagline: "I build things for web",
+  img: profile,
+  about: `I am a passionate Front-End Web Developer dedicated to creating engaging, responsive, and user-centric web experiences. With a strong foundation in HTML, CSS, and JavaScript, and expertise in frameworks like React and Next.js, I specialize in designing and implementing visually appealing and functional interfaces that enhance usability and performance.
+My creative approach and attention to detail ensure every project is both innovative and efficient, delivering seamless digital experiences that align with user needs and business goals. I thrive on exploring emerging technologies and modern tools to stay at the forefront of web development, bringing fresh ideas and solutions to each project.
+Whether it’s optimizing a website for speed, crafting intuitive navigation, or implementing dynamic features, I am committed to excellence, collaboration, and bringing designs to life with precision and care. Let's build something extraordinary together!`,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/in/samuel-bello-0b6884183/",
+  github: "https://github.com/Olamide0202/",
+  twitter: "https://x.com/SamuelBell5166",
+  instagram: "https://www.instagram.com/i_am_cornerstonecreative/",
+};
+
+// Enter your Work Experience here
+export const workDetails = [
+  {
+    Position: "Frontend Web Developer",
+    Company: `Ascent Hub`,
+    Location: "Ogun State",
+    Type: "Full Time",
+    Duration: "May 2021 - Dec 2021",
+  },
+  {
+    Position: "Internship",
+    Company: `Peter Akinola Foundation`,
+    Location: "Ogun State",
+    Type: "Internship",
+    Duration: "Feb 2022 - Dec 2022",
+  },
+  {
+    Position: "Internship",
+    Company: `Ogun Tech Hub`,
+    Location: "Ogun State",
+    Type: "Internship",
+    Duration: "June 2023 - Dec 2023",
+  },
+];
+
+// Enter your Education Details here
+export const eduDetails = [
+  {
+    Position: "Frontend Development",
+    Company: "Udemy, YouTube, Google, Medium",
+    Location: "Online",
+    Type: "Full Time",
+    Duration: "Jan 2022 - Present",
+  },
+  {
+    Position: "Diploma in Computer Engineering",
+    Company: `Moshood Abiola Polytechnic`,
+    Location: "Ogun State",
+    Type: "Full Time",
+    Duration: "Aug 2020 - Present",
+  },
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  redux: redux,
+  sass: sass,
+  tailwind: tailwind,
+  bootstrap: bootstrap,
+  vscode: vscode,
+  postman: postman,
+  npm: npm,
+  git: git,
+  github: github,
+  figma: figma,
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "Gallivant Blog",
+    image: projectImage1,
+    description: `Gallivant Blog is a vibrant travel hub designed to inspire, inform, and ignite your passion for exploring the world. where wanderlust meets storytelling!`,
+    techstack: "ReactJS",
+    previewLink: "https://gallivantblog.netlify.app/",
+    githubLink: "https://github.com/Olamide0202/Gallivant-Blog",
+  },
+  {
+    title: "Project title 2",
+    image: projectImage2,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 3",
+    image: projectImage3,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 4",
+    image: projectImage4,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 5",
+    image: projectImage5,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 6",
+    image: projectImage6,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "bellosamuel737@gmail.com",
+  phone: "+2347062870077",
+};
